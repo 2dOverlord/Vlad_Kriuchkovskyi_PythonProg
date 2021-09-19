d61910c94ba3b601efcd8c27c5c589ed2691f7ef
Vladyslav Kriuchkovskyi <74546413+2dOverlord@users.noreply.github.com>
2021-09-19 22:51:47 +0300

    Update PR1_var_11.py

--- PR1_var_11.py
+++ PR1_var_11.py
@@ -1,34 +1,73 @@
-def main_func():
-    array = []
-    n = int(input("Enter size of array(natural number): "))
-
-    for _ in range(n):
-        num = input(f"Enter element {_} of array(natural number): ")
-        array.append(int(num))
-
-    k = int(input("Enter k number(natural number, <n): "))
-
-    if k >= n:
-        raise ValueError("K MUST BE LESS THEN N")
-
-    max_avg = 0
-    index = -1
-
-    for zero_index in range(n-k+1):
-        avg = sum(array[zero_index:zero_index+k]) / len(array[zero_index:zero_index+k])
-        if avg > max_avg:
-            index = zero_index
-            max_avg = avg
-
-    print(f"Max arithmetical mean value is {max_avg}. Index of the first element is {index}")
+from task_class import BaseTask
+from custom_exceptions import ValidationError
 
 
-if __name__ == "__main__":
-    while True:
+class Pr1Task(BaseTask):
+    def __init__(self):
+        super().__init__(task_name='Pr1 11th variant',
+                         task_description="""
+                            Дано масив  натуральних чисел та число  (k<n). 
+                            Знайти послідовних елементів масиву, середнє арифметичне яких є максимальним 
+                            (вивести індекс першого елемента цієї послідовності і середнє арифметичне).
+                            """)
+
+    def input(self) -> dict:
         try:
-            main_func()
-            break
+            n = int(input('Enter the natural number n: '))
+            array = []
+
+            for _ in range(n):
+                num = input(f"Enter element {_} of array(natural number): ")
+                array.append(int(num))
+
+            k = int(input("Enter k number(natural number, k < n): "))
+
+            self.validate_args(n, array, k)
+            values_dict = {
+                'n': n,
+                'array': array,
+                'k': k
+            }
+            self.__dict__.update(values_dict)
+
+            return values_dict
         except ValueError:
-            print("Something gone wrong with input values, lets try again")
-        except ZeroDivisionError:
-            print("Something gone wrong with input values, lets try again")
+            print('Please, enter a number next time')
+        except ValidationError as val_err:
+            print(val_err)
+
+    def solve(self) -> tuple:
+        max_avg = 0
+        index = -1
+
+        for zero_index in range(self.n - self.k + 1):
+            avg = sum(self.array[zero_index:zero_index + self.k]) / len(self.array[zero_index:zero_index + self.k])
+            if avg > max_avg:
+                index = zero_index
+                max_avg = avg
+
+        print(f'Index of the first element in a row is {index} and maximum arithmetical mean is {max_avg}')
+
+        return max_avg, index
+
+    @staticmethod
+    def validate_args(n: int, array: list, k: int):
+        if n <= 0:
+            raise ValidationError('N must be > 0')
+        if k >= n:
+            raise ValidationError('k must be < n')
+        for num in array:
+            if num <= 0:
+                raise ValidationError('nums in array must be > 0')
+
+
+if __name__ == '__main__':
+    while True:
+        command = input('type exit if you want to stop code, if you want to run task type run: ')
+        if command == 'run':
+            task = Pr1Task()
+            task.run()
+        elif command == 'exit':
+            exit()
+        else:
+            print('Unknown command')
